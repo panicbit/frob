658580129c70473a62ad2bfbc836b8ce218e732f
panicbit <panicbit.dev@gmail.com>
2023-01-06 01:59:00 +0100

    add completions

--- frob/src/main.rs
+++ frob/src/main.rs
@@ -1,5 +1,8 @@
+use std::io;
+
 use anyhow::*;
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap_complete::Shell;
 
 #[derive(Parser)]
 pub enum Cli {
@@ -9,6 +12,10 @@ pub enum Cli {
     Monitor(frob_monitor::Cli),
     #[clap(subcommand)]
     Volume(frob_volume::Cli),
+    #[clap(hide = true)]
+    Completions {
+        shell: clap_complete::Shell,
+    },
 }
 
 fn main() -> Result<()> {
@@ -18,5 +25,12 @@ fn main() -> Result<()> {
         Cli::Brightness(cli) => frob_brightness::run(&cli),
         Cli::Monitor(cli) => frob_monitor::run(&cli),
         Cli::Volume(cli) => frob_volume::run(&cli),
+        Cli::Completions { shell } => generate_completions(shell),
     }
+}
+
+fn generate_completions(shell: Shell) -> Result<()> {
+    clap_complete::generate(shell, &mut Cli::command(), "frob", &mut io::stdout());
+
+    Ok(())
 }
